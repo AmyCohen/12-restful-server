4e9eeddbaba94473418b20c576e8251b1cfac78b
Amy Cohen <anbcohen@gmail.com>
2018-07-25 09:14:41 -0700

    created some fabric to populate the Map

--- src/main/java/models/FabricStorage.java
+++ src/main/java/models/FabricStorage.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FabricStorage {
+    public static Map<Integer, Fabric> fabric;
+
+    static  {
+        fabric = new HashMap<>();
+        Fabric doctorWho = new Fabric("knit", "cotton-lycra", "Knitorious Fabric", "Doctor Who", true);
+        Fabric lilly = new Fabric("knit", "French Terry", "SoSewEnglish Fabrics", "Charcoal Blush Lilly", false);
+        Fabric deadpool = new Fabric("woven", "PUL", "MadHouse Custom Fabrics", "Deadpool Argyle", true);
+
+        fabric.put(doctorWho.id, doctorWho);
+        fabric.put(lilly.id, lilly);
+        fabric.put(deadpool.id, deadpool);
+    }
 }
