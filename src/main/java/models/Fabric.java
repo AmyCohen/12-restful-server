3316e13fa54b432fe9be77f1a75a317061de0532
Amy Cohen <anbcohen@gmail.com>
2018-07-25 11:07:32 -0700

    added the API for Fabric

--- src/main/java/models/Fabric.java
+++ src/main/java/models/Fabric.java
@@ -4,18 +4,18 @@ public class Fabric {
     private static int REF_ID = 1000;
 
     public int id;
+    public String name;      //liverpool, scuba-crepe, dr-who, star-wars
     public String category;  //knit, woven
     public String base;      //polyester, cotton, poly-lycra, cotton-lycra
     public String bestStore; //SoSewEnglish, Knitorious, SlyFoxFabrics
-    public String name;      //liverpool, scuba-crepe, dr-who, star-wars
     public Boolean isCustom;
 
     public Fabric (String category, String base, String bestStore, String name, Boolean isCustom) {
         this.id = REF_ID++;
+        this.name = name;
         this.category = category;
         this.base = base;
         this.bestStore = bestStore;
-        this.name = name;
         this.isCustom = isCustom;
     }
 
--- src/main/java/server/FabricHTML.java
+++ src/main/java/server/FabricHTML.java
@@ -54,9 +54,4 @@ public class FabricHTML {
 
         return "editFabrics";
     }
-
 }
-
-/*
-
-*/
